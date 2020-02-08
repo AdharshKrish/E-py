9f0e8ac221681b50b40f3e137d7e97878cdc469c
Adharsh <adharshkrish@outlook.com>
2020-02-08 21:26:56 +0530

    nothing works

--- app/src/main/java/in/epy/app/MainActivity.java
+++ app/src/main/java/in/epy/app/MainActivity.java
@@ -2,13 +2,39 @@ package in.epy.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button service;
+    Toolbar tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        service=findViewById(R.id.button2);
+        tb=findViewById(R.id.toolbar);
+//        tb.inflateMenu(R.menu.menu);
+        setActionBar(tb);
+        service.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Categories.class));
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
