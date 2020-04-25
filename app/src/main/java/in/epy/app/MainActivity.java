9ba99c1dedbaa849e0ded0f5ed5958a892c74e59
PAVITHRAN S <pavithran200001@gmail.com>
2020-04-25 18:06:27 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/in/epy/app/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/in/epy/app/MainActivity.java
+++ app/src/main/java/in/epy/app/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setSupportActionBar(findViewById(R.id.toolbar));
+        Toolbar tb=findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
         tb.inflateMenu(R.menu.menu);
 
         product = findViewById((R.id.imageButtonProduct));
@@ -40,13 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
 
             }
-        });
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+
     }
 }
